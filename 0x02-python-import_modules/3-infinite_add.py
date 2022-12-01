e72e23351f30f5d2b379a418e94751086ee0a27d
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-02 00:51:07 +0100

    fixed
    fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+import sys
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    sum = 0
+    for i in range(1, argc):
+        num = int(sys.argv[i])
+        sum += num
+    print(sum)
