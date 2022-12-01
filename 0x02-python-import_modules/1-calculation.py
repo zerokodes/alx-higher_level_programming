c192164f27763c48daeee9e95f2609bf63a97f98
zerokodes <henrysam140@gmail.com>
2022-12-01 14:53:34 -0800

    fixed bug

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,6 @@ if _name_ == "_main_":
 
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a,b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
