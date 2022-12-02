0426bdeabf907689001ce1917b65140e18f15bb3
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-02 01:43:15 +0100

    fixed bug

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,3 +1,7 @@
-fjfkjdskljfgjkdfkjk
-jkdfjdfjhjfof
-kljdjkfs
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
