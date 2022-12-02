d53b7a539f8c0b9c803189fbb3d0e2b44be5c574
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-02 01:51:50 +0100

    fixed bug

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,2 @@
-djfbaj,erb.liserf.lerfcjkd
-aseycbddkfcnkfsd
-aCBH sd,hkb,sdjc 
-sdlbc shkdjbc sd
-casyhdailbcilusdb]
-edhbs,hkbd cskjh clsd
-sdckshdb fck
+#!/usr/bin/python3
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
