4111a3688860aab325bedd27f95e22e1b74eff9d
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-21 01:46:39 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
-shdbflkasbhdabdjbdvjvjd
-sadgjvdsljhbksdbgl,ks bd,dbgbdfbvdjhs
-sdahdbvhbdjkgvdj, bkdbkdh
-dsdhvdukgbjdbhdidkjbkdhbduj,hbds
-dgvdkujh
+#!/usr/bin/python3
+""" Module with a class thet defines a square"""
+
+
+class Square:
+    """Defines a square object
+    """
+    def __init__(self, size):
+        """Initializes the size
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
