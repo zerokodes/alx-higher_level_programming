f7d60663f93fcf2b84ad562cd4ab8a186e8ca234
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-21 01:00:25 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,15 @@
-shdblbafkysbfkibi,kdblisndkskd
-dksbdkh,sdbkldb jsbvhdjhdk,sd
-sdhlsdb,khbd,jash dbilhd.lakmsdshbds
-sdyifldiykbsldnkdbildjkak.dj;ikdkl.s
-dkljshd.olsjndosjdnjd
-sdjldhol.sdp;
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    length = 0
+
+    for i in my_list:
+        length += 1
+    for j in range(0, x):
+        try:
+            print("{:d}".format(my_list[j]), end='')
+            count += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return count
