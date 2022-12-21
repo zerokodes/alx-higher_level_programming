5d532a4c2cd3c5b773c748355afa3e4985fcaabd
Nduka Henry Chisom <henrysam140@gmail.com>
2022-12-21 01:47:10 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,3 +1,14 @@
-blhashb,kdbkagjdv,jdhvladbkhdvlgjds
-sdvjldb.hdb;dubd j fbalegfigkbr;frfnljgilrfzsd
-sjfbsdrjkfslfkd,f.sijojbeosepbjsfrerfhrtyhedbdshyjktgjfgukujeiooidkjeupr
+#!/usr/bin/python3
+"""defines a square """
+
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size=0):
+        """initializes the size"""
+        self.__size = size
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
