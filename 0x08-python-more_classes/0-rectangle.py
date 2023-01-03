040e8386c91d6efedabfae404a6d617cec8383d3
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-03 03:23:29 +0100

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,11 @@
-rkauygraygyuvkuevkaugyaekaeygakyee
-aekavyebkauebgydgvkybeykyebkdyhekydiie
-ekvedgvyujvdkyegvdkuywebdkywebdkydkue
-weakyegakgykawawkawekywegkwekybwe
+#!/usr/bin/python3
+"""
+    Defines an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """
+        Class that defines an empty Rectangle class
+     """
+    pass
