e7aa866cd1ac2f47bb1b5d2799dbc81740288173
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-09 22:24:45 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,12 @@
-dshsusdhjdbjxdk,
+#!/usr/bin/python3
+""" this module contains a function that
+    returns all attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ return all available
+        attributes and methods an object
+        param: obj(object) - object to get attribtes and methods from.
+    """
+    return dir(obj)
