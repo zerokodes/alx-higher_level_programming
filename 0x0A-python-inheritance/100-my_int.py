c183de83c740c42dd1ee9819d1c8a558116ef72c
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-09 22:23:31 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,17 @@
-seugydgy
+#!/usr/bin/python3
+
+""" defines a class MyInt that inherits from int
+    and overloads the == and != operators.
+"""
+
+
+class MyInt(int):
+    """overloads the == and != operators """
+
+    def __eq__(self, num):
+        """ return False if equal"""
+        return self.real != num
+
+    def __ne__(self, num):
+        """ return True if equal"""
+        return self.real == num
