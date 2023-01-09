0ef2fb8a21a91a076de5b449dbeb08fb4611f3cb
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-09 22:29:09 +0100

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,2 +1,18 @@
-skehhuisd
-dshik
+#!/usr/bin/python3
+
+""" This module defines a class based on 6-base_geometry.py ."""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """unimplemneted area method """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
