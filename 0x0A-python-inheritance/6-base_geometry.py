6e1812474e72c34aaf90506c688593ece31d527f
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-09 22:28:29 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,11 @@
-sdkihs
-dshi
+#!/usr/bin/python3
+
+""" This module defines a class based on 5-base_geometry.py ."""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """unimplemneted area method """
+        raise Exception("area() is not implemented")
