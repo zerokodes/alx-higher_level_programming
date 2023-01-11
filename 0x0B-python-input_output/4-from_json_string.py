9607eae63b969873f059d33b649e1cd3a5bc767d
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-11 21:52:49 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
-wkiegege
+#!/usr/bin/python3
+""""
+    Defines a function that returns an object(python data struct)
+    represented by a JSON string.
+"""
+
+
+def from_json_string(my_obj):
+    """ returns a python object from a JSON string"""
+    import json
+
+    return json.loads(my_obj)
