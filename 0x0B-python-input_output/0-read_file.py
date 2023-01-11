b3b37944d73494db3e593a25b017122786887228
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-11 21:37:44 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
-shjmshsjsjh
+#!/usr/bin/python3
+
+""" This module defines a function that reads a text
+    file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """ Read a UTF8 text file and
+        print it to stdout.
+        param: filename - the file to open and read.
+    """
+    with open(filename) as f:
+        text = f.read()
+        for t in text:
+            print(t, end='')
