d144c9001e9106cc28f8112d17b2250b3d83958f
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-11 21:58:15 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
-sulhslaushsau
+#!/usr/bin/python3
+
+"""defines a function that returns the dictionary
+    description
+"""
+
+
+def class_to_json(obj):
+    """returns a dictionary decription of a simple data structure"""
+    return obj.__dict__
