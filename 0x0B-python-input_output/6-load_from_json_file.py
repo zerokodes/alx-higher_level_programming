a75630e94cd462b5a960f464308fdab2e69d9999
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-11 21:55:14 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
-yagawu,kgukw,
+#!/usr/bin/python3
+""" This module defines a function that creates a python object
+    from a JSON file.
+"""
+
+
+def load_from_json_file(filename):
+    """Creates a python Object from a JSON file"""
+    import json
+    with open(filename) as f:
+        return json.load(f)
