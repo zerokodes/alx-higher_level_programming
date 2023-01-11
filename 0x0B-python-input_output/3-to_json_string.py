de65c94a48cf4da6311d03f26e45cf72ee149351
Nduka Henry Chisom <henrysam140@gmail.com>
2023-01-11 21:50:32 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,11 @@
+#!/usr/bin/python3
+""""
+    Defines a function that returns the JSON representationof an object
+"""
 
-whowwhohywohwwow
+
+def to_json_string(my_obj):
+    """ returns a JSON representation of an object"""
+    import json
+
+    return json.dumps(my_obj)
