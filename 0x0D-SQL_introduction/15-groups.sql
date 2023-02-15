718574f10fcf0b758ec45197a299825d8083ab1c
Nduka Henry Chisom <henrysam140@gmail.com>
2023-02-15 21:29:14 +0100

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,1 +1,6 @@
-dichnsudnhlosnl,
+-- list the number of records with same score in the second_table
+-- Records are sorted in descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
