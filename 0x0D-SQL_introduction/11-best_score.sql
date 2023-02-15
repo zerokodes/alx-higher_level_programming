1b45f49034c6ee899502b2eb60c556f29c7d457d
Nduka Henry Chisom <henrysam140@gmail.com>
2023-02-15 21:24:17 +0100

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,5 @@
-df.b,jhbc,jhbd,dbknhlnh/ils
+-- list all records with score >=10 in the second_table of the database hbtn_0n_0
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
