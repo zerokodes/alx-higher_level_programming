9c0bae6808b594db187862bff185bc74be762b8f
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-14 01:06:19 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
-djsfhkf
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
