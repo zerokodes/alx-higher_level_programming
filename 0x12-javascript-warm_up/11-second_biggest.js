503b3b9dd031d133a8355e3f0b692655dc0aae9a
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-14 01:03:28 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,12 @@
-ckdkcn,k
+#!/usr/bin/node
+
+const size = process.argv.length;
+
+if (size <= 3) {
+  console.log(0);
+} else {
+  const newA = process.argv.map(Number);
+  newA.slice(2, size);
+  newA.sort((a, b) => a - b);
+  console.log(newA[newA.length - 2]);
+}
