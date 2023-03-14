3007e89f59fa4a2374ea8db419c2a9727edaf602
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-14 01:10:39 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
-aubaukdna
+#!/usr/bin/node
+
+const size = process.argv[2];
+let charc = '';
+if (process.argv.length < 3 || isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    charc = '';
+    for (let j = 0; j < size; j++) charc += 'X';
+    console.log(charc);
+  }
+}
