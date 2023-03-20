0793d8f8c8f03d49efb05a93bd9eb03893b95e93
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-20 21:35:40 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,23 @@
-,k
+#!/usr/bin/python3
+"""prints the state object with the name passed as argument from the DB"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).order_by(State.id)
+    found = 0
+    for state in result:
+        if state.name == sys.argv[4]:
+            print('{}'.format(state.id))
+            found = 1
+            break
+    if found == 0:
+        print("Not found")
