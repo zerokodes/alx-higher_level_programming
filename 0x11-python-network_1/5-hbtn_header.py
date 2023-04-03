1f15d65f59aca95571f4f8c473e6466066b49998
Nduka Henry Chisom <henrysam140@gmail.com>
2023-04-03 15:12:53 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the value of
+    the variable X-Request-Id in the response header.
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
-"""
-import requests
 from sys import argv
-
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    headers = r.headers.get('X-Request-Id')
-    print(headers)
+import requests
 
 if __name__ == "__main__":
-    main(argv)
+    url = argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
