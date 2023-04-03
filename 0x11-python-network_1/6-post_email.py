be4520fb3937fa55523cb2907ec8f8882795d768
Nduka Henry Chisom <henrysam140@gmail.com>
2023-04-03 15:11:27 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
+""" takes URL and Email as arguments. sends a POST request to the
+    passed URL with the email as parameter. and finally displays the
+    body of the response.
 """
-POST request to the passed URL with the email as a parameter
-"""
+
 import requests
 from sys import argv
 
-
-def main(argv):
-    """
-    Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    """
-    values = {'email': argv[2]}
-    url = argv[1]
-    r = requests.post(url, data=values)
-    print(r.text)
-
 if __name__ == "__main__":
-    main(argv)
+    url = argv[1]
+    email = {'email': argv[2]}
+
+    r = requests.post(url, data=email)
+    print(r.text)
