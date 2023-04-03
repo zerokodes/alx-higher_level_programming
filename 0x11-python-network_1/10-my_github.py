9d12bab2c0c1737353540793546c45a84a7672e9
Nduka Henry Chisom <henrysam140@gmail.com>
2023-04-03 15:07:38 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-"""
-Module to access to the GitHub API and uses the information
-"""
+"""Takes my GitHub credentials and uses the GitHub API to display my id """
+
+from sys import argv
 import requests
 from requests.auth import HTTPBasicAuth
-from sys import argv
-
-
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
 
 if __name__ == "__main__":
-    main(argv)
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
