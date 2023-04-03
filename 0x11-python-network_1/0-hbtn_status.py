49f00f01d25c0d9bb22ff4ffe7022a49bde09709
Nduka Henry Chisom <henrysam140@gmail.com>
2023-04-03 15:16:29 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
-import urllib.request
-
-
-def main():
-    """
-    Funtion to print a response of a specific url
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as req
 
 if __name__ == "__main__":
-    main()
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
